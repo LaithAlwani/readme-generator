004a31cca333bc1212f6a1755e01afd33b37843a
Laith Alwani <laithalwani@gmail.com>
2021-01-25 13:34:38 -0500

    added comments

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+//inquirer
 inquirer.prompt([
     {
         type: 'input',
@@ -52,14 +53,10 @@ inquirer.prompt([
         name:'email'
     },
 ]).then(response =>{
-    console.log(response);
-    fs.writeFileSync('README.md',
+    fs.writeFileSync('README.md',  //writing to readme file
 `# ${response.title} 
 
 ### ${response.license} 
-
-## Description:
-    ${response.description}
 
 ## Table of Content:
 1.[Description](#Description)\n
@@ -68,6 +65,9 @@ inquirer.prompt([
 4.[Contribution Guidelines](#Contribution-Guidelines)\n
 5.[Test Instructions:](#Test-Instructions)\n
 6.[Questions](#Questions)\n
+
+## Description:
+    ${response.description}
 
 ## Installation Instructions:
     ${response.installation}
@@ -79,35 +79,16 @@ inquirer.prompt([
     ${response.contribution}
 
 ## License:
-    This application is covered under the ${response.license} license.    
+    This application is covered under the ${response.license}.    
 
 ## Test Instructions:
     ${response.test}
 
 ## Questions:
-    Github: [${response.github}](https://github.com/${response.github})
-    ${response.email}`
-
+Github: [${response.github}](https://github.com/${response.github})\n
+${response.email}`
+//chcking for errors
     , err=>{
         err ? console.log(err):"saved";
     });
 });
-
-
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, 
-// Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License 
-// that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
